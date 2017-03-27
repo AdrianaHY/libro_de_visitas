db04455bc19ce8d9e59c33322507923723182147
AdrianaHY <adriana.herya@gmail.com>
2017-03-26 23:13:39 -0600

    Segunda versión

---
+++ README.md
@@ -0,0 +1,15 @@
+### Ejercicio libro de visitas
+
+## HTML
+# Dentro del cuerpo puse input de tipo botón que servirán para modificar el estilo.
+# En cada botón de mandará llamar una función.
+# Después un área de texto en donde se escribirá un nuevo comentario, abajo un párrafo en donde se ve simultáneamente lo que se escribe.
+# Abajo botones para enviar y para borrar.
+# Arriba el área con los comentarios ya modificados.
+
+## JS
+# En el archivo js hay once funciones que servirán para modificar el el tamaño del texto, el color del fondo, el color de la tipografía y la alineación del párrafo.
+# Además de un botón para enviar y otro para borrar.
+
+## CSS
+## En este archivo sólo se le dará estilo a cada una de las partes del HTML.
--- assets/js/visitas.js
+++ assets/js/visitas.js
@@ -1,47 +1,64 @@
+
+//Declaro varibales globales en donde se obtienen los datos registrados en el área de texto y en la vista previa
+//de este modo se podrán ocupar en todas las funciones
 var comentarioModificado = document.getElementById("vistaAnterior");
 var mensaje = document.getElementById("contenido");
 
-
+//En ésta función duplico lo que se escribe en el área de texto con lo que se va viendo abajo y lo que se modifica
 function vistaPrevia(){
 
 comentarioModificado.innerHTML = mensaje.value;
 }
 
+//Para hacer el texto grande
 function textoGrande(){
   comentarioModificado.style.fontSize = "70px";
 }
-
+//Para hacer el texto mediano
 function textoMediano(){
   comentarioModificado.style.fontSize = "45px";
 }
-
+//Para hacer el texto más pequeño
 function textoChico(){
   comentarioModificado.style.fontSize = "30px";
 }
 
+//Para cambio de color en la tipografía
 function cambioColorTexto(){
-  var pedirColorHex = prompt("Elige un color de fuente : ");
+  //En esta línea pedimos al usuario que elija el color en hexadecimal
+  var pedirColorHex = prompt("Elige un color HEXADECIMAL de fuente : ");
+  //Aquí igualamos la variable a lo que el usuario escribió, además agrego el "#" para que no sea necesario que el usuario lo ponga
   pedirColorHex = "#" + pedirColorHex;
   document.getElementById("vistaAnterior").style.color = pedirColorHex;
 }
+
+//Para pedir color para cambiar el fondo
 function cambioColorFondo(){
-  var pedirColorFondo = prompt("Elige un color de fondo : ");
+  //En esta línea pedimos al usuario que elija el color en hexadecimal
+  var pedirColorFondo = prompt("Elige un color HEXADECIMAL de fondo : ");
+  //Aquí igualamos la variable a lo que el usuario escribió, además agrego el "#" para que no sea necesario que el usuario lo ponga
   pedirColorFondo = "#" + pedirColorFondo;
   document.getElementById("vistaAnterior").style.backgroundColor = pedirColorFondo;
 }
+
+//Para alinear el texto a la derecha
 function alinearALaDerecha(){
   comentarioModificado.style.textAlign = "right";
 
 }
+
+//Para alinear el texto a la izquierda
 function alinearALaIzquierda(){
   comentarioModificado.style.textAlign = "left";
 
 }
-
+//Para alinear el texto en el centro
 function centrar(){
   comentarioModificado.style.textAlign = "center";
 
 }
+
+// ÉSTA NO FUNCIONÓ :(
 // function publicarComentario() {
 
 //     var comentarioPublicado = document.getElementById("vistaAnterior").cloneNode(true);
@@ -69,3 +86,7 @@ function publicarComentario() {
 	document.getElementById("nuevosComentarios").value = "";
 
 	}
+
+  function borrarVista(){
+    comentarioModificado.innerHTML = "";
+  }
